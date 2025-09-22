0114402a4818d4a108c8a6a183e6296fbb92c084
ayaanyousaf <ayaanyousaf@hotmail.com>
2025-09-22 19:23:22 -0400

    adjusted arrow positioning on timeline cards. (Fix #4)

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -1,13 +1,13 @@
-import { SectionFade } from "./ui/SectionFade"
+import { SectionFade } from "./ui/SectionFade";
 import { cn } from "../lib/utils";
 import SpringFade from "./ui/SpringFade";
 import SpringGrow from "./ui/SpringGrow";
 
 // Data for timeline cards
 const schools = [
-  { 
+  {
     id: 1,
-    name: "Ontario Tech University", 
+    name: "Ontario Tech University",
     logo: "/assets/OTU.png",
     date: "2022-2027",
     degree: "Bachelor of Science (Honours) in Computer Science Co-op",
@@ -18,7 +18,7 @@ const schools = [
   },
   {
     id: 2,
-    name: "Maxwell Heights Secondary School", 
+    name: "Maxwell Heights Secondary School",
     logo: "/assets/MHSS.png",
     date: "2018-2022",
     degree: "Ontario Secondary School Diploma",
@@ -27,7 +27,7 @@ const schools = [
     p3: "Volunteered for community",
     p4: "Created a 2D platformer game in C#",
   },
-]
+];
 
 // Card Text Component
 const CardText = ({ school }) => {
@@ -35,7 +35,9 @@ const CardText = ({ school }) => {
     <div className="relative text-start w-full block">
       <div className="flex justify-between gap-2 items-start">
         <h3 className="text-lg md:text-2xl font-semibold">{school.name}</h3>
-        <p className="mt-1 text-sm md:text-base whitespace-nowrap">{school.date}</p>
+        <p className="mt-1 text-sm md:text-base whitespace-nowrap">
+          {school.date}
+        </p>
       </div>
       <p className="mt-2 text-sm md:text-lg text-gray-400">{school.degree}</p>
       <ul className="mt-4 text-xs md:text-sm list-disc md:ml-8 space-y-2">
@@ -50,7 +52,7 @@ const CardText = ({ school }) => {
 
 // Timeline Card Component
 const Card = ({ school }) => {
-  const side = (school.id % 2) === 0 ? "right" : "left"; // keep track of which side to put the card on (desktop only)
+  const side = school.id % 2 === 0 ? "right" : "left"; // keep track of which side to put the card on (desktop only)
 
   return (
     <div className="mb-16 last:mb-0 relative">
@@ -58,11 +60,7 @@ const Card = ({ school }) => {
         {/* Left content (desktop only) */}
         <div className={cn("hidden md:block w-1/2 pr-8")}>
           {side === "left" && (
-            <SpringFade
-              direction="left"
-              delay={0.1}
-              threshold={0.5}
-            >
+            <SpringFade direction="left" delay={0.1} threshold={0.5}>
               <div
                 className={cn(
                   "relative gradient-border border-primary border-2 font-[Poppins] rounded-lg p-8 w-full"
@@ -71,7 +69,7 @@ const Card = ({ school }) => {
                 {/* Card Arrow (pointing right) */}
                 <span
                   className={cn(
-                    "absolute top-10 arrow-left scale-x-[-1] -right-[1rem]"
+                    "absolute top-9 arrow-left scale-x-[-1] -right-[1rem]"
                   )}
                 />
                 <CardText school={school} />
@@ -82,7 +80,7 @@ const Card = ({ school }) => {
 
         {/* Timeline Connector Circle */}
         <SpringGrow>
-          <div className="mt-6 md:mt-4 shrink-0 h-12 w-12 md:h-16 md:w-16 rounded-full flex items-center justify-center bg-white z-10">
+          <div className="mt-6 md:mt-4 shrink-0 h-12 w-12 mx-[0.1rem] md:mx-auto md:h-16 md:w-16 rounded-full flex items-center justify-center bg-white z-10">
             <img
               src={school.logo}
               alt={school.name}
@@ -107,15 +105,13 @@ const Card = ({ school }) => {
               >
                 {/* Card Arrow (pointing left) */}
                 <span
-                  className={cn(
-                    "absolute top-10 arrow-left -left-[1rem]"
-                  )}
+                  className={cn("absolute top-9 arrow-left -left-[1rem]")}
                 />
                 <CardText school={school} />
               </div>
             </SpringFade>
           </div>
-          
+
           {/* Desktop card for desktop specific animations */}
           {side === "right" && (
             <div className="hidden md:block">
@@ -131,9 +127,7 @@ const Card = ({ school }) => {
                 >
                   {/* Card Arrow (pointing left) */}
                   <span
-                    className={cn(
-                      "absolute top-10 arrow-left -left-[1rem]"
-                    )}
+                    className={cn("absolute top-9 arrow-left -left-[1rem]")}
                   />
                   <CardText school={school} />
                 </div>
@@ -148,14 +142,14 @@ const Card = ({ school }) => {
 
 export const EducationSection = () => {
   return (
-    <section id="education" className="py-24 px-4 relative"> 
+    <section id="education" className="py-24 px-4 relative">
       <div className="container mx-auto">
         <SectionFade animate="animate-fade-in-down" threshold={0.7}>
           <h2 className="font-[Poppins] text-4xl md:text-5xl font-semibold mb-12 text-center">
             My <span className="text-primary"> Education</span>
           </h2>
         </SectionFade>
-        
+
         {/* Timeline Container */}
         <div className="relative mx-auto pb-[4rem] pt-4 max-w-6xl">
           {/* Timeline rail */}
@@ -167,5 +161,5 @@ export const EducationSection = () => {
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
